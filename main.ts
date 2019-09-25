dfd1663d578da7a833111675b6fb49ef7261b7ff
ricknyan <nmryan@millersville.edu>
2019-09-24 21:00:25 -0400

    added loading & saving files.
    Created two methods. loadFile() & saveFile() to load, read, write, and save to a file.

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ $(function() {
 
 	$("#log").val("UML Terminal\n>help");
 	$("#command").focus();
-
+	
 	//do on event
 
 	//runs every time key is pressed when #command is focus
@@ -44,7 +44,7 @@ function help(cmd : string) {
 function doCommand(command : string){
 	let log : JQuery = $("#log");
 		
-	log.val(log.val() + "\n>" + command);
+	apdLog(command, log);
 
 	//reg expression for split to allow any number of spaces
 	command = command.toLocaleLowerCase();
@@ -65,8 +65,50 @@ function doCommand(command : string){
 				, log);
 			}
 		break;
+		//used to access loading a file.
+		case "loadfile":
+			$("#inputFile").removeClass('hidden');
+			$("#load").removeClass('hidden');
+		break;
 	}
 	log.scrollTop(log[0].scrollHeight);	
+}
+
+/**
+ * Loads and writes the text contents
+ * 		of a file into the html textarea.
+ */
+function loadFile() {
+	$("#text").removeClass('hidden');
+	$("#save").removeClass('hidden');
+
+	// Grabs the file selected from the file input button.
+	var File = (<HTMLInputElement>document.getElementById("inputFile")).files[0];
+
+	// Using a FileReader to read the contents of the file as regular text.
+	var fileReader = new FileReader();
+    fileReader.onload = function(fileLoadedEvent){
+		var textFromFile = fileLoadedEvent.target.result;
+		// Printing the text into the textarea.
+		(<HTMLInputElement>document.getElementById("text")).value = <string>textFromFile;
+	};
+	
+	fileReader.readAsText(File, "UTF-8");
+
+}
+
+/**
+ * Saves the text from the textarea into a new YAML file.
+ */
+function saveFile() {
+	var textContent = (<HTMLInputElement>document.getElementById("text")).value;
+
+	// The octet-stream indicates a binary file.
+	// The URI encoder will encode the UTF-8 text.
+	var uriContent = "data:application/octet-stream," + encodeURIComponent(textContent);
+	
+	// Creates a clickable link to either open or save the file that was just create.
+    document.getElementById("link").innerHTML = "<a href=" + uriContent + " download=\"diagram.yml\">click me</a>";
 }
 
 /** apdLog (string, JQuery textfield)
